2b456b3796875c0017fe0a8144055c5666657f67
ky-s <kyohei_86@hotmail.com>
2019-04-09 22:18:13 +0900

    とりあえず動くようになった

--- blackjack.jl
+++ blackjack.jl
@@ -30,7 +30,7 @@ function point(player::Player)
     p, aces = 0, 0
     for card in player.hands
         if card.number == 1
-            aces++
+            aces += 1
             continue
         elseif card.number >= 10
             p += 10
@@ -41,15 +41,56 @@ function point(player::Player)
 
     while aces > 0
         p + 11 > 21 ? p += 1 : p += 11
+        aces -= 1
     end
-    return p
+
+    p
+end
+
+isbust(point::Integer)           = point > 21
+show_hands(player::Player)       = println(player.hands, " : point = ", point(player), isbust(point(player)) ? "(busted)" : "")
+deal(player::Player, deck::Deck) = push!(player.hands, pop!(deck.cards))
+
+function dealer_do(dealer::Player, deck::Deck)
+    while point(dealer) < 17
+        deal(dealer, deck)
+    end
+end
+
+function check(player::Player, dealer::Player)
+    isbust(point(player)) && return println("you lose")
+    if point(player) > point(dealer)
+        println("you win")
+    elseif point(player) == point(dealer)
+        println("no contest")
+    else
+        println("you lose")
+    end
+end
+
+function player_input()
+    print("Stand[s], Hit[h] :")
+    k = readline()
+    k in ("s", "h") ? k : player_input()
+    k
 end
 
 function start()
     deck = Deck()
     player, dealer = Player(map(i -> pop!(deck.cards), 1:2)), Player(map(i -> pop!(deck.cards), 1:2))
-    @show player, dealer
-    @show player.hands, point(player)
+    show_hands(player)
+    while player_input() == "h"
+        deal(player, deck)
+        print("[player]")
+        show_hands(player)
+    end
+
+    dealer_do(dealer, deck)
+
+    println("[dealer]")
+    show_hands(dealer)
+
+    check(player, dealer)
 end
 
 start()
